77e0e724d66c44f63eebc4e1751b3e6babfae205
mohammad25253 <alphahk555@gmail.com>
2016-02-15 01:34:08 +0330

    Update setrank.lua

--- plugins/setrank.lua
+++ plugins/setrank.lua
@@ -40,7 +40,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'????? ???? ??? ??????? ??? : '..user_info_msgs..'\n\n'
-  text = text..'@Mr_Vigeo'
+  text = text..'@mohammad20162015'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
  send_msg(extra.receiver, extra.user..' ??? ?????? ???? ??? ???? ???.', ok_cb, false)
@@ -79,7 +79,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'????? ???? ??? ??????? ??? : '..user_info_msgs..'\n\n'
-  text = text..'@Mr_Vigeo'
+  text = text..'@mohammad20162015'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, '???? ??? ???? ??? ?? ????? ??? ???? ???.\n?? ????? ??? ??????? ????\n/info @username', ok_cb, false)
@@ -119,7 +119,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'????? ???? ??? ??????? ??? : '..user_info_msgs..'\n\n'
-  text = text..'@Mr_vigeo'
+  text = text..'@mohammad20162015'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -192,7 +192,7 @@ local function run(msg, matches)
   text = text..'??? ???? : '..msg.to.title..'\n'
      text = text..'???? ???? : '..msg.to.id
     end
- text = text..'\n\n@Mr_Vigeo'
+ text = text..'\n\n@mohammad20162015'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
